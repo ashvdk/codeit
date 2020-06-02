e5db6065f22053e8856c12f5c5c5fc6031294f9c
ashvdk <ashwinvdrk@gmail.com>
2020-06-02 21:12:11 +0530

    moving my nodejs code to github

---
+++ action.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+    console.log("YOu are in action");
+}
--- app.js
+++ app.js
@@ -4,19 +4,9 @@ const routes = require('./Routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-
-
 app.use(cors());
 app.use(bodyParser.json())
 app.use(routes);
-
-
-if(process.env.NODE_ENV == "production")
-{
-    app.use(express.static('code-app/build'));
-}
-
 
 mongoose.connect(`mongodb+srv://ashvdk:I4OqnhVtjKg0q4RJ@cluster0-5eb4q.mongodb.net/test?retryWrites=true&w=majority`)
 .then(result => {
